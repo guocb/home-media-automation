8a990d4434a65aaa6f355c7071e51d6da2737017
Guo, Congbin <guocongbin@gmail.com>
2016-04-16 23:30:39 -0700

    init version of bt5156 site link analyzer

---
+++ bin/analyze_bt5156_url.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+'''
+'''
+import argparse
+import json
+import sys
+import re
+import urllib
+
+from contextlib import closing
+
+
+def analyze(url):
+    with closing(urllib.urlopen(url)) as page:
+        html = page.read()
+    m = re.search('var thunder_url = "(.+?)";', html)
+    rc = {}
+    if not m:
+        return json.dumps({'message': 'Cannot find xunlei link in the URL!'})
+
+    return json.dumps({'message': 'OK', 'link':
+                       m.group(1).decode('gbk').encode('utf-8')})
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-u', '--url', required=True,
+                        help='The page url of bbs.bt5156.net')
+    return parser.parse_args(argv)
+
+def main(argv):
+    opts = parse_args(argv)
+    print analyze(opts.url)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
